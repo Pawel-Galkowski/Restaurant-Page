648ed677fce6131b4903d8488321a71875ac2ced
Pawel Galkowski <pawel.galkowski@capgemini.com>
2022-07-05 12:05:10 +0200

    update build

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,4 +1,12 @@
-const port:any = 8080
+// if same address as angular app
+// const port:any = 8080
 
-export const BaseURL = window.location.protocol + "//" + window.location.hostname + ':' + port +'/';
-export const imageUrl = window.location.protocol + "//" + window.location.hostname + ':' + port +'/';
+// export const BaseURL = window.location.protocol + "//" + window.location.hostname + ':' + port +'/';
+// export const imageUrl = window.location.protocol + "//" + window.location.hostname + ':' + port +'/';
+
+// if other host
+
+const hostUrl:string = "https://store-database-app.herokuapp.com/dishes"
+
+export const BaseURL = hostUrl
+export const imageUrl = hostUrl
